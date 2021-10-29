ee94037a2aeac636841c7610e9dc667f5246b59e
IGOR <kondrahov.2000@mail.ru>
2021-10-29 13:20:12 +0300

    Changed pom.xml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ import parser.*;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main {
